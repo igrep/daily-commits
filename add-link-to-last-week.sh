2a39bac24d80ffb5a3974fa8a98ee85d4c46b3d0
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2020-11-08 20:56:39 +0900

    Add a script to add link to last week commit

---
+++ add-link-to-last-week.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eu
+
+last_week_commit="$(git log --before=1.day --format=format:%H -1 yesterday.md)"
+
+sed -i -e "s|^\[先週の記録はこちら\](.*)$|[先週の記録はこちら](https://github.com/igrep/daily-commits/blob/$last_week_commit/yesterday.md)|" yesterday.md
